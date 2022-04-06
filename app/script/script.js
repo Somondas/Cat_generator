0d405af8658a1f9953aa7c7a8e8b8c9f7a0a3580
Somon Das <somondas067@gmail.com>
2022-04-06 08:56:18 -0700

    First commit

---
+++ app/script/script.js
@@ -0,0 +1,7 @@
+function catgenerate(){
+    var image = document.createElement("img")
+    var div = document.getElementById("cat-gen")
+    image.src = "app/images/tenor.gif"
+    div.appendChild(image)
+
+}
